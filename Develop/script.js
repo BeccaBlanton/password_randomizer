c56e9a1dd106590cd53fbcffbcbc6c7af1ef35f0
Rebecca Blanton <beccaablanton@gmail.com>
2020-10-10 10:02:23 -0700

    starting function for generate password

--- Develop/script.js
+++ Develop/script.js
@@ -16,11 +16,9 @@ generateBtn.addEventListener("click", writePassword);
 //Start of testing
 
 //Arrays of character types
-
 var lowercaseABC=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseABC=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var otherChar = [ "!", "#", "$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"]
-
 var number = Math.floor(Math.random()*10);
 
 var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
@@ -31,12 +29,22 @@ console.log(number)
 console.log(randomLetter)
 console.log(randomCharacter)
 
+function generatePassword(){
+  passwordLength = prompt("Please pick your password length between 8-128 characters long");
+    return (passwordlength);
+  addNum = prompt("Do you need at least one Numeral?");
+    return(true);
+
+  addChar = prompt("Would you want to add Special Characters?");
+return(true)
+}
+
 //once Presses button, give prompts of options for criteria
 
 //criteria options for password
 
 //prompt for length of password 8-128 characters
-passwordLength = prompt("Please pick your password length between 8-128 characters long")
+
 //prompt for character types:  lowercase, uppercase, numeric, and/or special characters
 
 //make sure at least one character type is chosen.
