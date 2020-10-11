22f435e3022fae8ec0a4c3a835d2e74485743115
Rebecca Blanton <beccaablanton@gmail.com>
2020-10-11 13:13:09 -0700

    large array created for pulling random characters depending on selectors chosen for grabbing random characters. deleted unnecessary arrays

--- Develop/script.js
+++ Develop/script.js
@@ -18,37 +18,49 @@ generateBtn.addEventListener("click", writePassword);
 //Arrays of character types
 var letters=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var otherChar = [ "!", "#", "$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
-var number = Math.floor(Math.random()*10);
+var number = [0,1,2,3,4,5,6,7,8,9]
 
 var randomLetter = letters[Math.floor(Math.random() * letters.length)]
 
 var randomCharacter = otherChar[Math.floor(Math.random() * otherChar.length)]
-
-console.log(number)
-console.log(randomLetter)
-console.log(randomCharacter)
 
 function generatePassword(){
   var passArray =letters
 //once Presses button, give prompts of options for criteria
 
   var passwordLength = prompt("Please pick your password length between 8-128 characters long");
+  console.log(passwordLength)
 
   //prompt for character types:  lowercase, uppercase, numeric, and/or special characters
   var addNum = confirm("Do you want to add Numerals?");
   var addChar = confirm("Would you want to add Special Characters?");
 
-  if(addNum){
-    passArray = passArray.concat(number)
-  }
-  if(addChar){
-    passArray = passArray.concat(addChar)
-  }
+  console.log(addNum)
+  console.log(addChar)
+
+  //if(addNum){
+    //passArray = passArray.concat(number)
+  //}
+  //if(addChar){
+   // passArray = passArray.concat(addChar)
+  //}
   //password is generated based on conditions
-  for(var i = 0; i < length; i++){
-    console.log(passArray[Math.floor(Math.random() * letter.length)]
-    
-    )}
+
+  if(addNum === true) {
+    passArray = passArray.concat(number)
+    console.log(passArray)
+  }
+
+  if(addChar === true) {
+    passArray = passArray.concat(otherChar)
+    console.log(passArray)
+  }
+
+  for(var i = 0; i < passwordLength; i++){
+    console.log(passArray[Math.floor(Math.random() * passwordLength.join)])
+
+  return passwordText = (passArray[Math.floor(Math.random() * passwordLength)])
+    }
 }
 
 
